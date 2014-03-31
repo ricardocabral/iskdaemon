2c6a4d72710ebb3df9e797471b47934644091e38
Ricardo Cabral <ricardo@isnotworking.com>
2014-03-31 11:56:28 -0300

    bumping versin

--- src/setup.py
+++ src/setup.py
@@ -107,7 +107,7 @@ def find_data_files(d):
 
 print "#################################### Installing"
 setup(name="isk-daemon",
-      version='0.9.4',
+      version='0.9.5',
       description="Server and library for adding content-based (visual) image searching to any image related website or software.",
       long_description=long_description,
       keywords = "imgseek iskdaemon image cbir imagedatabase isk-daemon database searchengine",
